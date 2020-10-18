9e01e497f173050e5104b1dab157e6c3b7c9bbd3
codefornia <codeforniadreamin@gmail.com>
2020-10-18 23:21:34 +0300

    feat: function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){
+        n *= -1;
+    }
+    let reverseNumber = 0;
+    for(let i = n; i > 0; i=(i-i%10)/10) {
+        reverseNumber += i%10;
+        if(i-(i%10)>0) {
+            reverseNumber *= 10;
+        }
+    }
+    return reverseNumber;
 }
